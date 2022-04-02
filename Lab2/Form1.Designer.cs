76ffb77b06c1bb6674922e033bbcad71e08e97bd
913-Diaconu-Ana <daie2909@scs.ubbcluj.ro>
2022-04-02 15:12:28 +0300

    020422 - lab2 done

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TypeGrid = new System.Windows.Forms.DataGridView();
+            this.AntiquityGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TypeGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AntiquityGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TypeGrid
+            // 
+            this.TypeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TypeGrid.Location = new System.Drawing.Point(42, 45);
+            this.TypeGrid.Name = "TypeGrid";
+            this.TypeGrid.RowHeadersWidth = 51;
+            this.TypeGrid.RowTemplate.Height = 24;
+            this.TypeGrid.Size = new System.Drawing.Size(356, 283);
+            this.TypeGrid.TabIndex = 0;
+            // 
+            // AntiquityGrid
+            // 
+            this.AntiquityGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AntiquityGrid.Location = new System.Drawing.Point(503, 45);
+            this.AntiquityGrid.Name = "AntiquityGrid";
+            this.AntiquityGrid.RowHeadersWidth = 51;
+            this.AntiquityGrid.RowTemplate.Height = 24;
+            this.AntiquityGrid.Size = new System.Drawing.Size(812, 283);
+            this.AntiquityGrid.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(284, 71);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AntiquityGrid);
+            this.Controls.Add(this.TypeGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TypeGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AntiquityGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TypeGrid;
+        private System.Windows.Forms.DataGridView AntiquityGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection myConnection;
+        SqlDataAdapter parentDA, childDA;
+        DataSet myDataSet;
+        SqlCommandBuilder myCommandBuilder;
+        BindingSource childBindingSource, parentBindingSource;
+        
+        string parentName = ConfigurationManager.AppSettings["parentTable"];
+        string childName = ConfigurationManager.AppSettings["childTable"];
+        string childFK = ConfigurationManager.AppSettings["childFK"];
+        string parentFK = ConfigurationManager.AppSettings["parentFKCol"];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parentDA.Update(myDataSet, $"{parentName}");
+            childDA.Update(myDataSet, $"{childName}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myConnection = new SqlConnection(getConnectionString());
+            myDataSet = new DataSet();
+
+
+            childDA = new SqlDataAdapter($"select * from {childName}", myConnection);
+            parentDA = new SqlDataAdapter($"select * from {parentName}", myConnection);
+            myCommandBuilder = new SqlCommandBuilder(childDA);
+
+            childDA.Fill(myDataSet, $"{childName}");
+            parentDA.Fill(myDataSet, $"{parentName}");
+
+            DataRelation myDataRelation = new DataRelation($"FK_{childName}_{parentName}",
+                myDataSet.Tables[$"{parentName}"].Columns[$"{parentFK}"], myDataSet.Tables[$"{childName}"].Columns[$"{childFK}"]);
+            myDataSet.Relations.Add(myDataRelation);
+
+            childBindingSource = new BindingSource();
+            parentBindingSource = new BindingSource();
+
+            parentBindingSource.DataSource = myDataSet;
+            parentBindingSource.DataMember = $"{parentName}";
+
+            childBindingSource.DataSource = parentBindingSource;
+            childBindingSource.DataMember = $"FK_{childName}_{parentName}";
+
+            TypeGrid.DataSource = parentBindingSource;
+            AntiquityGrid.DataSource = childBindingSource;
+        }
+    }
+}
