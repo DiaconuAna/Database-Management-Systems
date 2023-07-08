7dbbb0e0d4aa659b382d019b55fef65c7c96f3c8
913-Diaconu-Ana <daie2909@scs.ubbcluj.ro>
2023-07-09 02:31:32 +0300

    add pr exam

---
+++ Practical Exam/Pr exam/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.dgvCarShop = new System.Windows.Forms.DataGridView();
+            this.Update = new System.Windows.Forms.Button();
+            this.Select = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarShop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(504, 90);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 51;
+            this.dgvEmployees.RowTemplate.Height = 24;
+            this.dgvEmployees.Size = new System.Drawing.Size(424, 266);
+            this.dgvEmployees.TabIndex = 0;
+            // 
+            // dgvCarShop
+            // 
+            this.dgvCarShop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarShop.Location = new System.Drawing.Point(21, 90);
+            this.dgvCarShop.Name = "dgvCarShop";
+            this.dgvCarShop.RowHeadersWidth = 51;
+            this.dgvCarShop.RowTemplate.Height = 24;
+            this.dgvCarShop.Size = new System.Drawing.Size(374, 265);
+            this.dgvCarShop.TabIndex = 1;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(504, 400);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(259, 61);
+            this.Update.TabIndex = 2;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Select
+            // 
+            this.Select.Location = new System.Drawing.Point(133, 400);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(262, 59);
+            this.Select.TabIndex = 3;
+            this.Select.Text = "Select";
+            this.Select.UseVisualStyleBackColor = true;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Car Shops";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Employees";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 501);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.dgvCarShop);
+            this.Controls.Add(this.dgvEmployees);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarShop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.DataGridView dgvCarShop;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Select;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Practical Exam/Pr exam/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection myConnection;
+        SqlDataAdapter CarshopDA, EmployeeDA;
+        DataSet myDataSet;
+        SqlCommandBuilder myCommandBuilder;
+        BindingSource CarShopBindingSource, EmployeeBindingSource;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            myConnection = new SqlConnection(@"Data Source = DESKTOP-MP1GSHH; Initial Catalog = practicalexam; Integrated Security = True");
+            myDataSet = new DataSet();
+            CarshopDA = new SqlDataAdapter("select * from CarShops", myConnection);
+            EmployeeDA = new SqlDataAdapter("select * from Employees", myConnection);
+            myCommandBuilder = new SqlCommandBuilder(EmployeeDA); // the table used in UPDATE above
+
+            CarshopDA.Fill(myDataSet, "CarShops"); //parent table
+            EmployeeDA.Fill(myDataSet, "Employees"); //child table
+
+            DataRelation myDataRelation = new DataRelation("FK_CarShops_Employees",
+                 myDataSet.Tables["CarShops"].Columns["Id"],
+                 myDataSet.Tables["Employees"].Columns["CarShopId"]);
+            myDataSet.Relations.Add(myDataRelation);
+
+            CarShopBindingSource = new BindingSource(); // PARENT
+            EmployeeBindingSource = new BindingSource(); // CHILD
+
+            CarShopBindingSource.DataSource = myDataSet;
+            CarShopBindingSource.DataMember = "CarShops";
+
+            EmployeeBindingSource.DataSource = CarShopBindingSource;
+            EmployeeBindingSource.DataMember = "FK_CarShops_Employees";
+
+
+            dgvCarShop.DataSource = CarShopBindingSource;
+            dgvEmployees.DataSource = EmployeeBindingSource;
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            EmployeeDA.Update(myDataSet, "employees");
+
+        }
+    }
+}
